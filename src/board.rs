7d171973e8ccdb2038e528895ae62d84bee93212
Spencer Gouw <swyverng55@g.ucla.edu>
2021-11-04 22:53:41 -0700

    First commit. minimax AI finished, some primitive playing.

---
+++ src/board.rs
@@ -0,0 +1,262 @@
+use std::fmt;
+use std::io;
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Board {
+    state: [[Square; 3]; 3],
+    turn: Turn,
+}
+
+impl Board {
+    pub fn new() -> Self {
+        Board {
+            state: [
+                [Square::Empty, Square::Empty, Square::Empty],
+                [Square::Empty, Square::Empty, Square::Empty],
+                [Square::Empty, Square::Empty, Square::Empty],
+            ],
+            turn: Turn::X,
+        }
+    }
+    pub fn from_state(state: [[Square; 3]; 3]) -> Self {
+        Board { state, turn: Turn::X }
+    }
+    fn key_to_coords(key: i8) -> (usize, usize) {
+        match key {
+            1 => (0, 0),
+            2 => (0, 1),
+            3 => (0, 2),
+            4 => (1, 0),
+            5 => (1, 1),
+            6 => (1, 2),
+            7 => (2, 0),
+            8 => (2, 1),
+            9 => (2, 2),
+            _ => unreachable!()
+        }
+    }
+    fn get_square(&self, i: usize, j: usize) -> Square {
+        self.state[i][j]
+    }
+    fn with_set_square(board: &Board, i: usize, j: usize, square: Square) -> Self {
+        let mut b = *board;
+        b.state[i][j] = square;
+        b
+    }
+    pub fn try_move(board: &Board, key: i8, turn: Turn) -> Option<Board> {
+        match key {
+            1..=9 => {
+                let (i, j) = Board::key_to_coords(key);
+                if board.get_square(i, j) != Square::Empty {
+                    None
+                } else {
+                    Some(Board::with_set_square(board, i, j, turn.to_square()))
+                }
+            }
+            _ => None
+        }
+    }
+    pub fn turn(&self) -> Turn {
+        let mut count = 0;
+        for row in self.state {
+            for col in row {
+                match col {
+                    Square::Empty => (),
+                    _ => count += 1,
+                }
+            }
+        }
+        if count % 2 == 0 {
+            Turn::X
+        } else {
+            Turn::O
+        }
+    }
+    pub fn succ(&self, turn: Turn) -> Vec<Board> {
+        let mut moves = Vec::new();
+        match turn {
+            Turn::X => {
+                for (i, row) in self.state.iter().enumerate() {
+                    for (j, col) in row.iter().enumerate() {
+                        match col {
+                            Square::Empty => moves.push(Self::with_set_square(&self, i, j, Square::X)),
+                            _ => (),
+                        }
+                    }
+                }
+            }
+            Turn::O => {
+                for (i, row) in self.state.iter().enumerate() {
+                    for (j, col) in row.iter().enumerate() {
+                        match col {
+                            Square::Empty => moves.push(Self::with_set_square(&self, i, j, Square::O)),
+                            _ => (),
+                        }
+                    }
+                }
+            }
+        }
+        moves
+    }
+    // Accept if game state is over or if all squares exhausted.
+    pub fn accepts(&self) -> bool {
+        // Evaluate it to any turn.
+        if self.evaluate(Turn::X) != 0 {
+            return true;
+        } else {
+            for row in self.state {
+                for col in row {
+                    match col {
+                        Square::Empty => return false,
+                        _ => (),
+                    }
+                }
+            }
+            return true;
+        }
+    }
+    pub fn evaluate(&self, turn: Turn) -> i8 {
+        if self.row_same(turn.to_square()) || self.col_same(turn.to_square()) || self.diag_same(turn.to_square()) {
+            1
+        } else if self.row_same(turn.other().to_square()) || self.col_same(turn.other().to_square()) || self.diag_same(turn.other().to_square()) {
+            -1
+        } else {
+            0
+        }
+    }
+    pub fn outcome(&self) -> String {
+        if self.accepts() {
+            if self.evaluate(Turn::X) == 1 {
+                "Player 1 (X) wins!".into()
+            } else if self.evaluate(Turn::O) == 1 {
+                "Player 2 (O) wins!".into()
+            } else {
+                "Draw.".into()
+            }
+        } else {
+            "Game over.".into()
+        }
+    }
+    fn row_same(&self, square: Square) -> bool {
+        for row in self.state {
+            if Self::three_same(row[0], row[1], row[2], square) {
+                return true;
+            }
+        }
+        false
+    }
+    fn col_same(&self, square: Square) -> bool {
+        for i in 0..3 {
+            if Self::three_same(self.state[0][i], self.state[1][i], self.state[2][i], square) {
+                return true;
+            }
+        }
+        false
+    }
+    fn diag_same(&self, square: Square) -> bool {
+        Self::three_same(self.state[0][0], self.state[1][1], self.state[2][2], square) ||
+        Self::three_same(self.state[2][0], self.state[1][1], self.state[0][2], square)
+    }
+    fn three_same(s1: Square, s2: Square, s3: Square, square: Square) -> bool {
+        s1 == s2 && s2 == s3 && s3 == square
+    }
+    // TODO: Might be best to get rid of this dynamic feature, display a guide board at the start, and simply reprimand users if they give an invalid move.
+    pub fn display(&self) -> io::Result<()> {
+        let mut board_chars = Vec::new();
+        let mut pos = 1;
+        for row in self.state {
+            for col in row {
+                match col {
+                    Square::X => board_chars.push("X".to_string()),
+                    Square::O => board_chars.push("O".to_string()),
+                    Square::Empty => {
+                        board_chars.push(pos.to_string());
+                        pos += 1;
+                    },
+                }
+            }
+        }
+
+        use io::Write;
+        let mut f = io::stdout();
+        writeln!(
+            f,
+            "{} | {} | {}",
+            board_chars[0], board_chars[1], board_chars[2]
+        )?;
+        writeln!(f, "---------")?;
+        writeln!(
+            f,
+            "{} | {} | {}",
+            board_chars[3], board_chars[4], board_chars[5]
+        )?;
+        writeln!(f, "---------")?;
+        writeln!(
+            f,
+            "{} | {} | {}",
+            board_chars[6], board_chars[7], board_chars[8]
+        )?;
+        Ok(())
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{} | {} | {}",
+            self.state[0][0], self.state[0][1], self.state[0][2]
+        )?;
+        writeln!(f, "---------")?;
+        writeln!(
+            f,
+            "{} | {} | {}",
+            self.state[1][0], self.state[1][1], self.state[1][2]
+        )?;
+        writeln!(f, "---------")?;
+        writeln!(
+            f,
+            "{} | {} | {}",
+            self.state[2][0], self.state[2][1], self.state[2][2]
+        )
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum Turn {
+    X,
+    O,
+}
+
+impl Turn {
+    pub fn other(&self) -> Self {
+        match self {
+            Turn::X => Turn::O,
+            Turn::O => Turn::X,
+        }
+    }
+    pub fn to_square(&self) -> Square {
+        match self {
+            Turn::X => Square::X,
+            Turn::O => Square::O,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Square {
+    X,
+    O,
+    Empty,
+}
+
+
+impl fmt::Display for Square {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Square::X => write!(f, "X"),
+            Square::O => write!(f, "O"),
+            Square::Empty => write!(f, " "),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,191 @@
+mod board;
+mod search;
+
+use crate::board::*;
+use rand::seq::SliceRandom;
+use std::cmp::{max, min};
+use std::collections::HashMap;
+use std::io::{Read, Stdin};
+
+// struct Game {
+//     current_turn: Turn, //btw, current turn can be determined from game state alone
+//     history: Vec<Board>,
+// }
+
+struct TicTacToeAI {
+    turn: Turn,
+    state_evals: HashMap<Board, i8>,
+}
+
+impl TicTacToeAI {
+    fn new() -> Self {
+        Self {
+            turn: Turn::X,
+            state_evals: HashMap::new(),
+        }
+    }
+    fn from_turn(turn: Turn) -> Self {
+        Self {
+            turn: turn,
+            state_evals: HashMap::new(),
+        }
+    }
+    // * The AI have been altered so that the only maximize if the board is playing their turn; otherwise, they choose the minimizing move, as though they were playing for their opponent.
+    fn choose_move(&mut self, board: Board) -> Board {
+        self.eval(board, board.turn());
+        let next = board.succ(board.turn());
+
+        let mut instant_wins = Vec::new();
+        let mut wins = Vec::new();
+        let mut draws = Vec::new();
+        let mut losses = Vec::new();
+
+        for n in next.into_iter() {
+            if n.evaluate(self.turn) == 1 {
+                instant_wins.push(n);
+            } else if let Some(e) = self.state_evals.get(&n) {
+                match e {
+                    1 => wins.push(n),
+                    0 => draws.push(n),
+                    -1 => losses.push(n),
+                    _ => unreachable!(),
+                }
+            } else {
+                panic!("State not found in evals: \n{}", n);
+            }
+        }
+
+        // ? Delete these debug prints later.
+        // instant_wins.iter().for_each(|b| println!("instant win: \n{}", b));
+        // wins.iter().for_each(|b| println!("win: \n{}", b));
+        // draws.iter().for_each(|b| println!("draw: \n{}", b));
+        // losses.iter().for_each(|b| println!("loss: \n{}", b));
+
+        // TODO: Not only choose from the winning moves, but choose the winning move which leads to the most winning outcomes.
+        if !instant_wins.is_empty() {
+            *instant_wins.choose(&mut rand::thread_rng()).unwrap()
+        } else if !wins.is_empty() {
+            *wins.choose(&mut rand::thread_rng()).unwrap()
+        } else if !draws.is_empty() {
+            *draws.choose(&mut rand::thread_rng()).unwrap()
+        } else {
+            *losses.choose(&mut rand::thread_rng()).unwrap()
+        }
+    }
+    fn eval(&mut self, board: Board, t: Turn) -> i8 {
+        if self.turn == t {
+            self.minimax_evaluate(board, 100, true)
+        } else if self.turn == t.other() {
+            self.minimax_evaluate(board, 100, false)
+        } else {
+            unreachable!();
+        }
+    }
+    fn minimax_evaluate(&mut self, board: Board, depth: u8, maximizing: bool) -> i8 {
+        if let Some(&eval) = self.state_evals.get(&board) {
+            return eval;
+        }
+
+        if depth == 0 || board.accepts() {
+            let eval = board.evaluate(self.turn);
+            self.state_evals.insert(board, eval);
+            return eval;
+        }
+
+        let mut eval: i8;
+        if maximizing {
+            let mut maxeval = i8::MIN;
+            for next in board.succ(self.turn) {
+                eval = self.minimax_evaluate(next, depth - 1, false);
+                maxeval = max(maxeval, eval);
+            }
+            self.state_evals.insert(board, maxeval);
+            maxeval
+        } else {
+            let mut mineval = i8::MAX;
+            for next in board.succ(self.turn.other()) {
+                eval = self.minimax_evaluate(next, depth - 1, true);
+                mineval = min(mineval, eval);
+            }
+            self.state_evals.insert(board, mineval);
+            mineval
+        }
+    }
+}
+
+// TODO: Implement actual game and game loop.
+
+
+fn print_move(board: Board, who_moved: String) {
+    println!("\n{} moved: \n========\n{}========", who_moved, board)
+}
+
+fn receive_move(board: Board, turn: Turn) -> Option<Board> {
+    println!("Your move.");
+    let mut input = String::new();
+    while let Ok(_) = std::io::stdin().read_line(&mut input) {
+        if let Ok(key) = input.trim().parse::<i8>() {
+            match key {
+                1..=9 => {
+                    match Board::try_move(&board, key, turn) {
+                        None => {
+                            println!("Invalid move... try again, but choose an empty square.");
+                            continue;
+                        }
+                        some_board => return some_board,
+                    }
+                }
+                0 => return None,
+                _ => {
+                    println!("Invalid number given... try 1-9! Or 0 to quit.");
+                }
+            }
+        } else {
+            println!("Invalid command.  Are you even giving a number?  Try again.");
+        }
+        input.clear();
+    }
+    None
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let mut p1 = TicTacToeAI::from_turn(Turn::X);
+    let mut p2 = TicTacToeAI::from_turn(Turn::O);
+    // let s: Board = Board::from_state([
+    //     [Square::X, Square::Empty, Square::Empty],
+    //     [Square::Empty, Square::Empty, Square::O],
+    //     [Square::Empty, Square::Empty, Square::Empty],
+    // ]);
+    // println!("{}", s);
+    // let eval1 = p1.eval(s, Turn::X);
+    // let eval2 = p2.eval(s, Turn::X);
+    // println!("{}, {}", eval1, eval2);
+    // let a = p1.choose_move(s);
+    // println!("{}", a);
+
+    let mut board: Board = Board::new();
+    board.display()?;
+    println!("Offer a number from 1-9 accordingly to place your square.  Offer 0 to end the game.");
+    while !board.accepts() {
+        if let Some(b) = receive_move(board, Turn::X) {
+            print_move(b, "Human: X".into());
+            std::thread::sleep(std::time::Duration::from_secs(1));
+            // b.display()?;
+
+            if b.accepts() {
+                break;
+            }
+
+            board = p2.choose_move(b);
+            print_move(board, "AI: O".into());
+            std::thread::sleep(std::time::Duration::from_secs(1));
+
+            // board.display()?;
+        } else {
+            break;
+        }
+    }
+    println!("{}", board.outcome());
+
+    Ok(())
+}
